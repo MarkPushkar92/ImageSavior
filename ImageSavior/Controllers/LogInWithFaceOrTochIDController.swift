f43e73690aae9269cc0cda0bf19a13b3d4e35015
MarkPushkar92 <mark.pushkar92@gmail.com>
2022-04-18 18:38:29 +1000

    face and touch ids applied

---
+++ ImageSavior/Controllers/LogInWithFaceOrTochIDController.swift
@@ -0,0 +1,8 @@
+//
+//  LogInWithFaceOrTochIDController.swift
+//  ImageSavior
+//
+//  Created by Марк Пушкарь on 18.04.2022.
+//
+
+import Foundation
---
+++ ImageSavior/Logic Stuff /LocalAuthorizationService.swift
@@ -0,0 +1,8 @@
+//
+//  LocalAuthorizationService.swift
+//  ImageSavior
+//
+//  Created by Марк Пушкарь on 18.04.2022.
+//
+
+import Foundation
